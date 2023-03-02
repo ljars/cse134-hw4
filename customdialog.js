be17d839640a420475eecf7ec52afad50689d985
Lucas Jarstad <ljarstad@ucsd.edu>
2023-03-01 21:48:44 -0800

    Part 3 partially done.

--- customdialog.js
+++ customdialog.js
@@ -9,20 +9,48 @@ export function myConfirm(message, callback) {
     let dialog = document.getElementById('confirm-dialog');
     dialog.querySelector('p').innerText = message;
     dialog.show();
-    dialog.addEventListener('close', () => {
-        callback(dialog.returnValue === 'true');
-    });
+    if (dialog.getAttribute('my-listener-set') !== 'true') {
+        dialog.setAttribute('my-listener-set', 'true');
+        dialog.addEventListener('close', () => {
+            callback(dialog.returnValue === 'true');
+        });
+    }
 }
 
 export function myPrompt(message, callback) {
     let dialog = document.getElementById('prompt-dialog');
     dialog.querySelector('p').innerText = message;
     dialog.show();
-    dialog.addEventListener('close', () => {
-        if (dialog.returnValue === 'true') {
-            callback(dialog.querySelector('input').value);
-        } else {
-            callback(null);
-        }
-    });
+    if (dialog.getAttribute('my-listener-set') !== 'true') {
+        dialog.addEventListener('close', () => {
+            dialog.setAttribute('my-listener-set', 'true');
+
+            if (dialog.returnValue === 'true') {
+                callback(dialog.querySelector('input').value);
+            } else {
+                callback(null);
+            }
+        });
+    }
+}
+
+export function myBlogPrompt(message, callback) {
+    let dialog = document.getElementById('blog-prompt-dialog');
+    dialog.querySelector('p').innerText = message;
+    dialog.show();
+    if (dialog.getAttribute('my-listener-set') !== 'true') {
+        dialog.addEventListener('close', () => {
+            // Don't set duplicate listeners
+            dialog.setAttribute('my-listener-set', 'true');
+            if (dialog.returnValue === 'true') {
+                let answer = {};
+                answer.title = dialog.querySelector('input[name="title"]').value;
+                answer.date = dialog.querySelector('input[name="date"]').value;
+                answer.summary = dialog.querySelector('textarea[name="summary"]').value;
+                callback(answer);
+            } else {
+                callback(null);
+            }
+        });
+    }
 }
