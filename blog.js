e30497dc509709357c476e39d41d32d5a27ef544
Lucas Jarstad <ljarstad@ucsd.edu>
2023-03-02 23:43:14 -0800

    Separate modules

--- blog.js
+++ blog.js
@@ -1,6 +1,7 @@
 import DOMPurify from 'https://cdn.jsdelivr.net/npm/dompurify/dist/purify.es.min.js';
 
-import { myAlert, myConfirm, myPrompt, myBlogPrompt } from './customdialog.js';
+import { myConfirm, myBlogPrompt } from './customdialog.js';
+import { getPosts, load, save, addToList, removeFromList, updateInList } from './blogstorage.js';
 
 function sanitized(strs, val) {
     return DOMPurify.sanitize(strs[0] + val);
@@ -77,80 +78,10 @@ function addPostClicked() {
     });
 }
 
-let posts = [];
-let post1 = { title: "A Topic", date: "2023-03-01", summary: "Here is a blog post about a topic." };
-let post2 = { title: "Another Topic", date: "2023-02-27", summary: "This blog post is about another topic." };
-
-function load() {
-    try {
-        let hasBeenSaved = localStorage.getItem('hasBeenSaved');
-        if (!hasBeenSaved) {
-            posts = [];
-            posts.push(post1);
-            posts.push(post2);
-            showList();
-            return;
-        }
-        posts = JSON.parse(localStorage.getItem('postsArray'));
-        console.log("LOADED SUCCESS");
-        if (posts === null) {
-            posts = [];
-        }
-    }
-    catch (error) {
-        console.log("LOADED FAIL");
-        console.log(error);
-        posts = [];
-
-    }
-    showList();
-}
-function save() {
-    localStorage.setItem('postsArray', JSON.stringify(posts));
-    localStorage.setItem('hasBeenSaved', 'true');
-}
 function showList() {
-    posts.forEach(post => {
+    getPosts().forEach(post => {
         clonePost(post.title, post.date, post.summary);
     });
-}
-function addToList(answer) {
-    console.log('add to list');
-    let title = sanitized`${answer.title}`;
-    let date = sanitized`${answer.date}`;
-    let summary = sanitized`${answer.summary}`;
-    let p = { title: title, date: date, summary: summary };
-    posts.push(p);
-}
-function removeFromList(title, date, summary) {
-    console.log('remove from list');
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].title == title && posts[i].date == date && posts[i].summary == summary) {
-            posts.splice(i, 1);
-            return;
-        }
-    }
-}
-function updateInList(titleBefore, dateBefore, summaryBefore, answer) {
-    console.log('update in list');
-
-    let title = sanitized`${answer.title}`;
-    let date = sanitized`${answer.date}`;
-    let summary = sanitized`${answer.summary}`;
-
-    for (let i = 0; i < posts.length; i++) {
-        console.log(posts[i].title == titleBefore);
-        console.log(posts[i].title + ' ' + titleBefore);
-        if (posts[i].title == titleBefore && posts[i].date == dateBefore && posts[i].summary == summaryBefore) {
-            posts[i].title = title;
-            posts[i].date = date;
-            posts[i].summary = summary;
-            return;
-        }
-    }
-
-    console.log('no match found!');
-
 }
 
 function init() {
@@ -158,6 +89,7 @@ function init() {
     addPostButton.addEventListener('click', addPostClicked);
 
     load();
+    showList();
 
 }
 
